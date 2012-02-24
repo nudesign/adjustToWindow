41f37f0149a0bc5f3163404515a85b40b0db216d
rodrigo brancher <rbrancher@gmail.com>
2012-02-24 17:15:07 -0200

    first commit - v0.1

---
+++ jquery-adjustToWindow.js
@@ -0,0 +1,49 @@
+(function( $ ) {
+  $.fn.adjustToWindow = function(options) {
+    
+    var settings = $.extend({
+                              'centralizeW': true,
+                              'centralizeH': true
+                            }, options);
+        
+    return this.each(function (){
+
+      var object = $(this);
+    
+      var iW = object.data('width');
+      var iH = object.data('height');
+      var iRatio = iW / iH;
+
+      // if (iW === 0 || iH === 0) return;
+    
+      var wW = $(window).width();
+      var wH = $(window).height();
+      var wRatio = wW / wH;
+    
+      if (iRatio >= wRatio) {
+        var nH = wH;
+        var nW = Math.round(iRatio * nH);
+
+        if (settings.centralizeW === true) {
+          var mL = -Math.round(Math.abs(wW - nW) / 2);
+        } else {
+          mL = 0;
+        }
+        var mT = 0;
+      } else {
+        var nW = wW;
+        var nH = Math.round(nW / iRatio);
+
+        if (settings.centralizeH === true) {
+          var mT = -Math.round(Math.abs(wH - nH) / 2);
+        } else {
+          mT = 0;
+        }
+        var mL = 0;
+      }
+
+      object.css({'width': nW, 'height': nH, 'margin-top': mT, 'margin-left': mL});
+      
+    });
+  };
+})( jQuery );
